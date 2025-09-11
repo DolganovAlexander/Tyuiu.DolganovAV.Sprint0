8c3a5d0a4a07d7338a0ee5b2047f9fb80bd077d0
Dolganov Alexander <waitiliti@gmail.com>
2025-09-11 21:46:10 +0500

    Task7

---
+++ Tyuiu.DolganovAV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.DolganovAV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] NumOne, int[] NumTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = NumOne[i] + NumTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
